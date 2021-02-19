fa077d72bdbfc2adf2bb65b3731abf7b7c7ff19c
Elise Wetherholt <ewether@gmail.com>
2021-02-19 09:43:52 -0500

    added js animations to text

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,4 +1,31 @@
-let headerText = document.querySelector('.heading-two');
-let headerContent = headerText.textContent;
+// massage therapy text
 
-console.log(headerText);
+let text = document.querySelector('.heading-two');
+let headerText = text.textContent;
+
+const splitText = headerText.split("");
+text.textContent = "";
+console.log(splitText);
+
+for(let i=0; i < splitText.length; i++) {
+  text.innerHTML += "<span>" + splitText[i] + "</span>";
+}
+
+
+let char = 0;
+let timer = setInterval(onTick, 50);
+
+function onTick() {
+  const span = text.querySelectorAll('span')[char];
+  span.classList.add('fade');
+  char++;
+  if (char === splitText.length) {
+    complete();
+  }
+}
+
+function complete() {
+  clearInterval(timer);
+  timer = null;
+}
+
