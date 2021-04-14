f22a2e0724d444cee88f43df729cec6e3ef69a58
Elise Wetherholt <ewether@gmail.com>
2021-04-14 11:03:07 -0400

    update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,8 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.assets.compress = true
+
   config.assets.precompile += ['nav.js']
 
   config.assets.precompile += ['accordion.js']
